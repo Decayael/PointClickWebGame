670ce1992612ea0befc2392e75b463fe049e2b1a
Ricardo Creemers <r.creemers@gmail.com>
2024-02-22 13:03:22 +0100

    added dialog
    Removed background colors on hitboxes
    added dialog function
    -showMessage
    -hideMessage
    audio is triggered with those functions

--- js/main.js
+++ js/main.js
@@ -10,10 +10,18 @@ gameState = {
     ]
 }
 
+const sec = 1000;
+
 //Main Character
 const mainCharacter = document.getElementById("mainCharacter");
 const offsetCharacter = 16;
 
+//speech bubbles
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
+const counterSpeech = document.getElementById("counterSpeech");
+const counterAvatarImg = document.getElementById("counterAvatarImg");
+const mcAudio = document.getElementById("mcAudio");
+const cAudio = document.getElementById("cAudio");
 //Inventory
 const inventoryBox = document.getElementById("inventoryBox"); //div
 const inventoryList = document.getElementById("inventoryList"); //ul
@@ -27,29 +35,27 @@ gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
-    if(e.target.id !== "mainCharacter") {
+    if (e.target.id !== "mcImage") {
         mainCharacter.style.left = x - offsetCharacter + "px";
         mainCharacter.style.top = y - offsetCharacter + "px";
     }
 
     console.log(e.target.id);
     switch (e.target.id) {
-        
+
         case "door1":
-            mainCharacter.style.backgroundColor = "#FFFF00";
-            door1.style.opacity = 0.5;
             sign.style.opacity = 1;
-            if(document.getElementById("key1") !== null){
+            if (document.getElementById("key1") !== null) {
                 console.log('Found key!');
                 document.getElementById("key1").remove();
                 changeInventory('key', 'add');
             }
-            
+
             break;
         case "door2":
-            if(gameState.door2locked == true) {
+            if (gameState.door2locked == true) {
                 // check if we have key
-                if(document.getElementById("inv-key") !== null) {
+                if (document.getElementById("inv-key") !== null) {
                     //yes -> unlock door?
                     gameState.door2locked = false;
                     changeInventory('key', 'delete');
@@ -62,19 +68,28 @@ gameWindow.onclick = function (e) {
             } else {
                 console.log('enter building');
             }
-            
+
             break;
 
         case "sign":
-            mainCharacter.style.backgroundColor = "#FFFF00";
+
             sign.style.opacity = 0.5;
-            door1.style.opacity = 1;
+
+            break;
+
+        case "statue":
+            showMessage(mainCharacterSpeech, mcAudio, "Wow cool statue..");
+            setTimeout(function () { counterAvatarImg.style.opacity = 1; }, 4 * sec);
+            setTimeout(showMessage, 4 * sec, counterSpeech, cAudio, "I can talk you know..dummy");
+            setTimeout(showMessage, 8 * sec, mainCharacterSpeech, mcAudio, "You don't have to be so mean.");
+            setTimeout(showMessage, 12 * sec, counterSpeech, cAudio, "You should check the north house..");
+            setTimeout(function () { counterAvatarImg.style.opacity = 0; }, 16 * sec);
             break;
 
         default:
             //explode
-            mainCharacter.style.backgroundColor = "#1286a7";
-            door1.style.opacity = 1;
+
+
             sign.style.opacity = 1;
             break;
 
@@ -89,18 +104,18 @@ gameWindow.onclick = function (e) {
  * @returns 
  */
 function changeInventory(itemName, action) {
-    if(itemName == null || action == null) {
+    if (itemName == null || action == null) {
         console.log('wrong parameters given to changeInventory()');
-        return 
+        return
     }
 
-    switch(action) {
+    switch (action) {
         case 'add':
             gameState.inventory.push(itemName);
             break
         case 'delete':
-            gameState.inventory.find(function(item, index){
-                if(item == itemName) {
+            gameState.inventory.find(function (item, index) {
+                if (item == itemName) {
                     var index = gameState.inventory.indexOf(item);
                     if (index !== -1) {
                         gameState.inventory.splice(index, 1);
@@ -120,9 +135,9 @@ function changeInventory(itemName, action) {
  * @param {Array} inventory array of items 
  * @param {HTMLElement} inventoryList html <ul> element 
  */
-function updateInventory(inventory, inventoryList){
+function updateInventory(inventory, inventoryList) {
     inventoryList.innerHTML = '';
-    inventory.forEach(function(item) {
+    inventory.forEach(function (item) {
         const inventoryItem = document.createElement("li");
         inventoryItem.id = "inv-" + item;
         inventoryItem.innerText = item;
@@ -130,3 +145,26 @@ function updateInventory(inventory, inventoryList){
     })
 }
 
+/**
+ * Shows a message in a speech bubble
+ * @param {getElementById} targetBalloon 
+ * @param {getElementById} targetSound 
+ * @param {string} message 
+ */
+function showMessage(targetBalloon, targetSound, message) {
+    targetSound.currentTime = 0;
+    targetSound.play();
+    targetBalloon.style.opacity = "1";
+    targetBalloon.innerText = message;
+    setTimeout(hideMessage, 4 * sec, targetBalloon, targetSound);
+}
+
+/**
+ * Set the opacity to 0
+ * @param {getElementById} targetBalloon 
+ * @param {getElementById} targetSound 
+ */
+function hideMessage(targetBalloon, targetSound) {
+    targetSound.pause();
+    targetBalloon.style.opacity = "0";
+}
