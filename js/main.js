016962477ec84faa679db941bb4f2edc79558eef
Decayael <144001697+Decayael@users.noreply.github.com>
2024-03-08 15:18:07 +0100

    Alles

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 document.getElementById("mainTitle").innerText = "Point and Click adventure game";
 
+
 //Game window reference
-const gameWindow = document.getElementById("gameWindow");
+const gameWindow = document.getElementById("gameWindow");6
 
 //Game state
-gameState = {
+let gameState = {
     "door2locked": true,
-    "inventory": [
-    ]
+    "inventory": []
 }
 
+if(typeof(Storage) !== "undefined"){
+    //code for localStorage
+    if(localStorage.gameState){
+
+        gameState = JSON.parse(localStorage.gameState);
+    }
+    else{
+        //save local gameState into browser storage
+        localStorage.setItem("gameState",JSON.stringify(gameState))
+    }
+}
+else {
+    alert('Web storage not supported!')
+}
 const sec = 1000;
 
 //Main Character
@@ -25,10 +39,12 @@ const cAudio = document.getElementById("cAudio");
 //Inventory
 const inventoryBox = document.getElementById("inventoryBox"); //div
 const inventoryList = document.getElementById("inventoryList"); //ul
+let ismap2 = false;
 
 //Foreground Items
 const door1 = document.getElementById("door1");
 const sign = document.getElementById("sign");
+let uwu = 0;
 
 
 gameWindow.onclick = function (e) {
@@ -44,11 +60,11 @@ gameWindow.onclick = function (e) {
     switch (e.target.id) {
 
         case "door1":
-            sign.style.opacity = 1;
             if (document.getElementById("key1") !== null) {
                 console.log('Found key!');
                 document.getElementById("key1").remove();
                 changeInventory('key', 'add');
+                showMessage(mainCharacterSpeech, mcAudio, "There seems to be a key in the keyhole. However, it doesn't fit this door...");
             }
 
             break;
@@ -59,38 +75,118 @@ gameWindow.onclick = function (e) {
                     //yes -> unlock door?
                     gameState.door2locked = false;
                     changeInventory('key', 'delete');
-                    console.log('Door unlocked!');
+                    showMessage(mainCharacterSpeech, mcAudio, "The door has been unlocked!");
 
                 } else {
                     //no -> alert 'door locked'
-                    alert("Door is locked!");
+                    showMessage(mainCharacterSpeech, mcAudio, "This door seems to be locked.");
                 }
-            } else {
-                console.log('enter building');
+            }
+                else {
+                    if(ismap2){
+                        console.log('exit building');
+                        document.getElementById("map1").style.display = "inline";
+                        document.getElementById("map2").style.display = "none";
+                        ismap2 = false;
+                    }
+                    else{
+                    // Door is unlocked
+                    console.log('enter building');
+                    document.getElementById("map1").style.display = "none";
+                    document.getElementById("map2").style.display = "inline";
+                    ismap2 = true
+                    }
+
             }
 
             break;
 
+
+            case "chest1":
+                if(uwu == 0){
+                showMessage(mainCharacterSpeech, mcAudio, "the chest is locked but there is no key hole");
+                setTimeout(showMessage, 4 * sec, mainCharacterSpeech, mcAudio, "I have to look for something to crack it open...");
+
+                }
+                else{
+                    //wow kist open
+                    changeInventory('Magic Stone', 'add');
+                }
+            if (uwu == 0) {            // check if we have dynamite and a lighter
+                if (document.getElementById("inv-dynamite") != null && document.getElementById("inv-lighter") != null) {
+                    //yes -> explode the chest
+                    uwu++;
+                    changeInventory('dynamite', 'delete');
+                    
+                    changeInventory('lighter', 'delete');
+                    showMessage(mainCharacterSpeech, mcAudio, "Better get out of here before I die!");
+                }
+                else{
+                    showMessage(mainCharacterSpeech, mcAudio, "i should check the graves for some items maybe")
+                }
+            }
+                break;
+            
+
         case "sign":
+            if (document.getElementById("dynamite1") !== null) {
+                    console.log('Found dynamite? Why is there dynamite on a grave?!');
+                    document.getElementById("dynamite1").remove();
+                    changeInventory('dynamite', 'add');
+                    showMessage(mainCharacterSpeech, mcAudio, "Found dynamite? Why is there dynamite on a grave?!")
+                }
+                break;
+        
+        case "lighter1":
+            if (document.getElementById("lighter1") !== null) {
+                    console.log('lighter found!');
+                    document.getElementById("lighter1").remove();
+                    changeInventory('lighter', 'add');
+                    showMessage(mainCharacterSpeech, mcAudio, "hey a lighter! i'll be taking that.")
+                }
+                break;
 
-            sign.style.opacity = 0.5;
+        case "arrow1":
+            console.log('enter new map')
+            document.getElementById("mainCharacter").style = "left: 0px; top: 120px;";
+            document.getElementById("map1").style.display = "none";
+            document.getElementById("map2").style.display = "none";
+            document.getElementById("map3").style.display = "inline";
 
+        break;
+
+        case "arrow2":
+            console.log('enter new map')
+            document.getElementById("mainCharacter").style = "left: 755px; top: 173px;";
+            document.getElementById("map1").style.display = "inline";
+            document.getElementById("map2").style.display = "none";
+            document.getElementById("map3").style.display = "none";
+
+        break;
+        
+        case "teleporter":
+        console.log('enter new map')
+            document.getElementById("mainCharacter").style = "left: 755px; top: 173px;";
+            document.getElementById("map1").style.display = "none";
+            document.getElementById("map2").style.display = "none";
+            document.getElementById("map3").style.display = "none";
+            document.getElementById("map4").style.display = "inline";
+            
             break;
 
+
         case "statue":
-            showMessage(mainCharacterSpeech, mcAudio, "Wow cool statue..");
+            showMessage(mainCharacterSpeech, mcAudio, "Wow cOoL statue...");
             setTimeout(function () { counterAvatarImg.style.opacity = 1; }, 4 * sec);
-            setTimeout(showMessage, 4 * sec, counterSpeech, cAudio, "I can talk you know..dummy");
+            setTimeout(showMessage, 4 * sec, counterSpeech, cAudio, "I can hear, you know...stupid");
             setTimeout(showMessage, 8 * sec, mainCharacterSpeech, mcAudio, "You don't have to be so mean.");
-            setTimeout(showMessage, 12 * sec, counterSpeech, cAudio, "You should check the north house..");
-            setTimeout(function () { counterAvatarImg.style.opacity = 0; }, 16 * sec);
+            setTimeout(showMessage, 12 * sec, counterSpeech, cAudio, "You should check the house North from here...");
+            setTimeout(showMessage, 18 * sec, counterSpeech, cAudio, "Also people tend to leave weird items behind for the afterlife of the deceased.");
+            setTimeout(function () { counterAvatarImg.style.opacity = 0; }, 24 * sec);
             break;
 
         default:
             //explode
-
-
-            sign.style.opacity = 1;
             break;
 
     }
@@ -167,4 +263,12 @@ function showMessage(targetBalloon, targetSound, message) {
 function hideMessage(targetBalloon, targetSound) {
     targetSound.pause();
     targetBalloon.style.opacity = "0";
+}
+
+/**
+ * store local gameState into LocalStorage.gameState
+ * @param {object} gameState our gameState object
+ */
+function saveToBrowser(gameState){
+    localStorage.gameState = JSON.stringify(gameState);
 }
